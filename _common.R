4d639f345bb0a9766f738e741136cb66a440b564
jrnold <jeffrey.arnold@gmail.com>
2017-04-20 08:59:38 -0700

    misc

--- _common.R
+++ _common.R
@@ -1,4 +1,9 @@
-library("knitr")
+suppressPackageStartupMessages({
+  library("knitr")
+  library("rstan")
+  library("tidyverse")
+})
+
 set.seed(92508117 )
 options(digits = 3)
 
@@ -13,12 +18,10 @@ knitr::opts_chunk$set(
   fig.show = "hold"
 )
 
-options(dplyr.print_min = 6, dplyr.print_max = 6)
+options(dplyr.print_min = 6,
+        dplyr.print_max = 6)
 
-suppressPackageStartupMessages({
-  library("rstan")
-})
-
+# Helpful Documentation functions
 rpkg_url <- function(pkg) {
   paste0("https://cran.r-project.org/package=", pkg)
 }
@@ -36,4 +39,14 @@ rdoc <- function(pkg, fun, full_name = FALSE) {
   paste0("[", text, "](", rdoc_url(pkg, fun), ")")
 }
 
-#knit_print.stanmodel <- function() {  }
+STAN_VERSION <- "2.14.0"
+STAN_URL <- "http://mc-stan.org/documentation/"
+STAN_MAN_URL <- paste0("https://github.com/stan-dev/stan/releases/download/v", STAN_VERSION, "/stan-reference-", STAN_VERSION, ".pdf")
+
+standoc <- function(x = NULL) {
+  if (!is.null(x)) {
+    STAN_MAN_URL
+  } else {
+    paste("[", x, "](", STAN_MAN_URL, ")")
+  }
+}
