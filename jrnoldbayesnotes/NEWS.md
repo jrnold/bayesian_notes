336305f853e4a8900672a69e96640cbc91b253aa
jrnold <jeffrey.arnold@gmail.com>
2018-04-20 21:24:55 -0700

    add federalist papers

---
+++ jrnoldbayesnotes/NEWS.md
@@ -0,0 +1,7 @@
+# jrnold.bayes.notes 0.1.0.9000
+
+* Added `federalist` and `federalist_wordcounts` datasets.
+* Added a `NEWS.md` file to track changes to the package.
+
+
+
---
+++ jrnoldbayesnotes/data-raw/federalist.R
@@ -0,0 +1,33 @@
+#' create federalist paper data
+suppressPackageStartupMessages({
+  library("tidyverse")
+  library("tidytext")
+  library("corpus")
+})
+data("federalist", package = "corpus")
+
+federalist <- federalist %>%
+  # add a document number
+  mutate(number = row_number())
+
+federalist_wordcounts <- term_counts(federalist) %>%
+  mutate(number = as.integer(text),
+         term = as.character(term)) %>%
+  select(-text) %>%
+  left_join(select(federalist, number, author), by = "number")
+
+functionwords <- readLines("data-raw/functionwords.txt")
+
+federalist_wordcounts <- federalist_wordcounts %>%
+  mutate(term = if_else(term %in% functionwords, term, "OTHER")) %>%
+  group_by(number, author, term) %>%
+  summarise(count = sum(count)) %>%
+  ungroup() %>%
+  complete(nesting(number, author), term, fill = list(count = 0L)) %>%
+  mutate(count = as.integer(count)) %>%
+  arrange(number, term)
+
+use_data(federalist_wordcounts, overwrite = TRUE)
+
+federalist <- select(federalist, -text)
+use_data(federalist, overwrite = TRUE)
